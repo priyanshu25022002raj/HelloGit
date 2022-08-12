308324cadef5a3c23119abc5dd279d4c0c3ce04b
unknown <priyanshuraj25022002@gmial.com>
2022-08-12 10:27:25 +0530

    minus

--- Hellogit.java
+++ Hellogit.java
@@ -4,7 +4,8 @@ public class Hellogit {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        int c=a+b;
+        int c=a-b;
 	System.out.println(c);
+
     }
 }
